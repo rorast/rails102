2dd3779cbc8a2639f570793912f002fc01670586
rorast <rorast.power.game@gmail.com>
2016-12-11 22:45:34 +0800

    check owner permission when access edit/update/destroy

--- rails102/app/controllers/groups_controller.rb
+++ rails102/app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -15,6 +15,9 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
 
@@ -31,7 +34,9 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -42,8 +47,11 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
     @group.destroy
-    flash[:alert] = "Group deleted"
+    redirect_to groups_path, alert: "Group deleted"
   end
 
   private
